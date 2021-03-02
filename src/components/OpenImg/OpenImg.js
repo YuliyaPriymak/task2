96502c17c11aca9f942d4e0d486ad2676d0c024b
YuliyaPriymak <bogatir.yuliya@gmail.com>
2021-03-02 20:12:48 +0200

    add &&

--- src/components/OpenImg/OpenImg.js
+++ src/components/OpenImg/OpenImg.js
@@ -6,19 +6,26 @@ class OpenImg extends Component {
     render() {
         const {closeImg, imgSrc} = this.props;
         return (
-            <div className='modal-desc'>
-                <div className='modal-box'>
-                    <div className='d-flex justify-content-end'>
-                        <button
-                            className='d-block btn btn-primary'
-                            onClick={closeImg}>close</button>
-                    </div>
-                    <div className='d-flex'>
-                        <img src={imgSrc} alt="image"></img>
-                    </div>
+            <div>
+                {!imgSrc && <div>Loading...</div>}
+                {!!imgSrc && (
+                    <div className='modal-desc'>
+                        <div className='modal-box'>
+                            <div className='d-flex justify-content-end'>
+                                <button
+                                    className='d-block btn btn-primary'
+                                    onClick={closeImg}>close
+                                </button>
+                            </div>
+                            <div className='d-flex'>
+                                <img src={imgSrc} alt="image"></img>
+                            </div>
 
-                </div>
+                        </div>
+                    </div>
+                )}
             </div>
+
         );
     }
 }
